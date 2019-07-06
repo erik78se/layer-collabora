32b36f6037414b2e14d9db83660fdf3ea8ec5b82
Erik Lönroth <erik.lonroth@gmail.com>
2019-07-07 00:03:15 +0200

    correct values, image and template

--- src/reactive/collabora.py
+++ src/reactive/collabora.py
@@ -1,5 +1,6 @@
 from charms.reactive import (
     when,
+    when_any,
     when_all,
     when_not,
     set_flag,
@@ -18,10 +19,11 @@ import requests
 
 #
 #
-# Configure collabora docker image =>
+# This charm configures the collabora docker image =>
 # https://www.collaboraoffice.com/code/docker/
 #
-#
+# * It defaults to listen to port:80
+# * It is intended to run behind a ssl-terminiation-fqdn charm
 
 # Installation
 
@@ -60,9 +62,6 @@ def run_container():
                    "--name", hookenv.application_name(),
                    "-p", ":{}:9980".format(p),
                    "-e", "domain={}".format(d),
-                   "-e",
-                   ('"extra_params=--o:ssl.enable=false '
-                    '--o:ssl.termination=true"'),
                    "--restart",
                    "always",
                    "--cap-add", "MKNOD", "collabora/code"]
@@ -78,17 +77,38 @@ def run_container():
 @when('collabora.started')
 @when_not('collabora.configured')
 def write_collabora_config():
+    """Configure the docker container when collabora isn't configured"""
+
     ctxt = {'domain': hookenv.config('nextcloud_domain'),
-            'ssl_enabled': hookenv.config('ssl_enabled'),
-            'termination_enabled': hookenv.config('termination_enabled')}
+            'ssl_enable': str(hookenv.config('ssl_enable')).lower(),
+            'ssl_termination': str(hookenv.config('ssl_termination')).lower()}
 
     render('loolwsd.xml', '/srv/loolwsd.xml', context=ctxt, perms=0o777)
+
     check_call(["docker", "cp",
                 "/srv/loolwsd.xml",
                 "{}:/etc/loolwsd/loolwsd.xml".format(
                     hookenv.application_name())])
+
     check_call(["docker", "restart", hookenv.application_name()])
+
+    # Remove temporary config file
+    check_call(["rm", "/srv/loolwsd.xml"])
+
     set_flag('collabora.configured')
+
+
+
+@when_any('config.changed.nextcloud_domain',
+          'config.changed.ssl_enable',
+          'config.changed.ssl_termination')
+def collabora_reconfigured():
+    """Let it be known that collabora no longer is configured."""
+
+    log("collabora detected a config changed and thus is condidered unconfigured.")
+
+    clear_flag('collabora.configured')
+
 
 
 @when('collabora.stop', 'docker.available')
